e9d2d9b1c30464731476ea331ef2d7e7cbe39107
Héctor Fernández <hector.fernandez@king.com>
2025-12-22 10:46:48 +0100

    wip emailservice tests

--- src/main/java/com/familymoney/familymoney/properties/MailSenderProperties.java
+++ src/main/java/com/familymoney/familymoney/properties/MailSenderProperties.java
@@ -2,9 +2,11 @@ package com.familymoney.familymoney.properties;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "mail.sender")
 @Validated
+@Builder
 public record MailSenderProperties(@NotBlank String name, @Email String email) {}
