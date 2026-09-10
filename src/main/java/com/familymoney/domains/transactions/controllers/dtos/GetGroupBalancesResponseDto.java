2958a2773eb517d1952a0a7e657dd94bc09739d6
Hector Fernandez <hector.fernandez@king.com>
2026-09-10 07:15:21 +0200

    .

--- src/main/java/com/familymoney/domains/transactions/controllers/dtos/GetGroupBalancesResponseDto.java
+++ src/main/java/com/familymoney/domains/transactions/controllers/dtos/GetGroupBalancesResponseDto.java
@@ -4,4 +4,10 @@ import java.util.Map;
 import java.util.UUID;
 import org.javamoney.moneta.Money;
 
+/**
+ * DTO for the response of getting group balances.
+ *
+ * @param balances A map of user IDs to their respective balances. A positive balance indicates that
+ *     the user is owed money, while a negative balance indicates that the user owes money.
+ */
 public record GetGroupBalancesResponseDto(Map<UUID, Money> balances) {}
